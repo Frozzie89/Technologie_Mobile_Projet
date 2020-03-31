04eef43a4033831c5d08bfd07804d5d850dc94dd
Nathan <nath223c@gmail.com>
2020-03-31 16:37:19 +0200

    Autoload pathing corrected
    Autoload prendra le chemin absolu en fonction de l'emplacement du fichier

--- inc/autoload.php
+++ inc/autoload.php
@@ -6,8 +6,9 @@ $extranet_url = "http://localhost:".$_SERVER["SERVER_PORT"]."/BTI/FAA/extranet/"
 $current_url = "localhost:".$_SERVER["SERVER_PORT"]."/".$_SERVER["REQUEST_URI"];
 
 function app_autoload($class){
-    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-    require "$root\\TI\\Technologie_Mobile_Projet\\class\\$class.php";
+    $path =  realpath("");
+    //echo "$path\\class\\$class.php";
+    require "$path\\class\\$class.php";
 }
 
 ini_set('display_errors',1);
