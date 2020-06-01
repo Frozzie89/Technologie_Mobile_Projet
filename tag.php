37d28bac0fbd468ff3efd53f6f28a7890bb660af
Nathan <nath223c@gmail.com>
2020-06-01 23:39:23 +0200

    Commit test pour VS

--- tag.php
+++ tag.php
@@ -2,15 +2,17 @@
 require "inc/header.php";
 require "inc/nav.php";
 
-$tagPage = $db->query("Select * from tags where id_tags = :id", ["id"=>$_GET['id']])->fetch();
-$NB_POST = count($db->query("Select * from posts where id_tags = :id", ["id"=>$_GET['id']])->fetchAll());
+$tagPage = $db->query("Select * from tags where id_tags = :id", ["id" => $_GET['id']])->fetch();
+$NB_POST = count($db->query("Select * from posts where id_tags = :id", ["id" => $_GET['id']])->fetchAll());
 $NB_PER_PAGE = 8;
 $OFFSET = 0;
 ?>
-
+/* Petit test avec VS */
 <!-- JS, jquery and stuff -->
 <script src="assets/js/jquery-3.4.1.js"></script>
-<script>window.jQuery || document.write('<script src="assets/js/jquery-3.4.1.js"><\/script>')</script>
+<script>
+    window.jQuery || document.write('<script src="assets/js/jquery-3.4.1.js"><\/script>')
+</script>
 <script src="assets/js/bootstrap.bundle.js"></script>
 <script src="assets/js/script.js"></script>
 
@@ -71,7 +73,7 @@ $OFFSET = 0;
     const NB_PER_PAGE = 2;
     let OFFSET = 0;
 
-    $(document).ready(function (){
+    $(document).ready(function() {
         $("#next-arrow").prop("disabled", true);
         $("#previous-arrow").prop("disabled", true);
         const csstendance = 'td';
@@ -82,21 +84,22 @@ $OFFSET = 0;
     });
 
     function limitTextPost(texte_posts) {
-        texte_posts = texte_posts.substring(0,50);
+        texte_posts = texte_posts.substring(0, 50);
         texte_posts += "...";
         return texte_posts;
     }
 
     function noPagination() {
         $.ajax({
-            url:"pagination.php",
-            method:"GET",
-            data:{  tagID : <?= $_GET['id'] ?>,
-                nbppage : NB_PER_PAGE,
-                offset : OFFSET
+            url: "pagination.php",
+            method: "GET",
+            data: {
+                tagID: <?= $_GET['id'] ?>,
+                nbppage: NB_PER_PAGE,
+                offset: OFFSET
             },
-            dataType:"json",
-            success:function(posts){
+            dataType: "json",
+            success: function(posts) {
                 displayPosts(posts);
             }
         });
@@ -105,20 +108,22 @@ $OFFSET = 0;
     function displayPosts(posts) {
         var styleCard = Cookies.get('theme') == 'dark' ? ['#1A1A1B', 'black'] : ['white', '#e1e3e1'];
         $('.evr').remove();
-        $.each(posts, function (i, post) {
-            $.getJSON('extranet/getPhotoOfPost.php', {postID: post.id_posts}, function (photo) {
+        $.each(posts, function(i, post) {
+            $.getJSON('extranet/getPhotoOfPost.php', {
+                postID: post.id_posts
+            }, function(photo) {
                 let postText = limitTextPost(post.texte_posts);
-                $("<div class='col-lg-3 card-news-max-height evr " + post.id_tags + "'><div class='card card-height card-style' style='background-color: "+ styleCard[0]+ "; color: "+ styleCard[1]+"'>" +
+                $("<div class='col-lg-3 card-news-max-height evr " + post.id_tags + "'><div class='card card-height card-style' style='background-color: " + styleCard[0] + "; color: " + styleCard[1] + "'>" +
                     "<a href='post.php?id=" + post.id_posts + "'><img class='card-img-top' src='extranet/" + photo.nom_photos + "' alt='ok'>" +
                     "<div class='card-body card-body-style'>" +
                     "<h5 class='card-title'>" + post.titre_posts + "</h5>" +
                     "<p class='card-text'>" + postText + "</p>" +
                     "<p class='tagPost'>" + post.id_tags + "</p>" +
-                    "<div class='tagLikesComments' style='position:absolute'>"+
-                    "<i class='far fa-arrow-alt-circle-up fa-2x'></i>"+
-                    "<h3 style='margin-left:5px'>"+ post.nbLikes +"</h3>"+
-                    "<i class='far fa-comments fa-2x' style='margin-left:15px'></i>"+
-                    "<h3 style='margin-left:5px'>" + post.nbComments +"</h3></div>").appendTo($('.post-every-post'));
+                    "<div class='tagLikesComments' style='position:absolute'>" +
+                    "<i class='far fa-arrow-alt-circle-up fa-2x'></i>" +
+                    "<h3 style='margin-left:5px'>" + post.nbLikes + "</h3>" +
+                    "<i class='far fa-comments fa-2x' style='margin-left:15px'></i>" +
+                    "<h3 style='margin-left:5px'>" + post.nbComments + "</h3></div>").appendTo($('.post-every-post'));
             })
         })
     }
@@ -126,40 +131,46 @@ $OFFSET = 0;
     function setUpPagination() {
         $("#next-arrow").prop("disabled", false);
         $.ajax({
-            url:"pagination.php",
-            method:"GET",
-            data:{  tagID : <?= $_GET['id'] ?>,
-                    nbppage : NB_PER_PAGE,
-                    offset : OFFSET
+            url: "pagination.php",
+            method: "GET",
+            data: {
+                tagID: <?= $_GET['id'] ?>,
+                nbppage: NB_PER_PAGE,
+                offset: OFFSET
             },
-            dataType:"json",
-            success:function(posts){
+            dataType: "json",
+            success: function(posts) {
                 displayPosts(posts);
             }
         });
     }
 
     function fetchDataTag(tagValue, selector, cssclass) {
-        $.getJSON('fetchPostTendance.php', {tagID: tagValue, tendance : false}, function (posts) {
+        $.getJSON('fetchPostTendance.php', {
+            tagID: tagValue,
+            tendance: false
+        }, function(posts) {
             var styleCard = Cookies.get('theme') == 'dark' ? ['#1A1A1B', 'black'] : ['white', '#e1e3e1'];
             NB_POST = posts.length;
-            $.each(posts, function (i, post) {
-                $.getJSON('extranet/getPhotoOfPost.php', {postID: post.id_posts}, function (photo) {
+            $.each(posts, function(i, post) {
+                $.getJSON('extranet/getPhotoOfPost.php', {
+                    postID: post.id_posts
+                }, function(photo) {
                     let postText = limitTextPost(post.texte_posts);
-                    $("<div class='col-lg-3 card-news-max-height td " + post.id_tags + "'><div class='card card-height card-style' style='background-color: "+ styleCard[0]+ "; color: "+ styleCard[1]+" '>"+
-                        "<a href='post.php?id=" + post.id_posts + "'><img class='card-img-top' src='extranet/" + photo.nom_photos + "' alt= " + post.titre_posts +" >" +
+                    $("<div class='col-lg-3 card-news-max-height td " + post.id_tags + "'><div class='card card-height card-style' style='background-color: " + styleCard[0] + "; color: " + styleCard[1] + " '>" +
+                        "<a href='post.php?id=" + post.id_posts + "'><img class='card-img-top' src='extranet/" + photo.nom_photos + "' alt= " + post.titre_posts + " >" +
                         "<div class='card-body card-body-style'>" +
                         "<h5 class='card-title'>" + post.titre_posts + "</h5>" +
                         "<p class='card-text'>" + postText + "</p>" +
                         "<p class='tagPost'>" + post.id_tags + "</p>" +
-                        "<div class='tagLikesComments' style='position:absolute'>"+
-                        "<i class='far fa-arrow-alt-circle-up fa-2x' ></i>"+
-                        "<h3 style='margin-left:5px'>"+ post.nbLikes +"</h3>"+
-                        "<i class='far fa-comments fa-2x' style='margin-left:15px'></i>"+
-                        "<h3 style='margin-left:5px'>" + post.nbComments+"</h3></div>").appendTo($('.post-tendance'));
+                        "<div class='tagLikesComments' style='position:absolute'>" +
+                        "<i class='far fa-arrow-alt-circle-up fa-2x' ></i>" +
+                        "<h3 style='margin-left:5px'>" + post.nbLikes + "</h3>" +
+                        "<i class='far fa-comments fa-2x' style='margin-left:15px'></i>" +
+                        "<h3 style='margin-left:5px'>" + post.nbComments + "</h3></div>").appendTo($('.post-tendance'));
                 })
             })
-            if (NB_POST <= NB_PER_PAGE){
+            if (NB_POST <= NB_PER_PAGE) {
                 noPagination();
             } else {
                 setUpPagination(NB_POST, NB_PER_PAGE, OFFSET);
@@ -171,13 +182,13 @@ $OFFSET = 0;
     $("#searchbarPost").on("input", function() {
         var search = this.value;
         $('.evr').hide().each(function() {
-            var text = $(this).find(".card-title:contains('"+search+"')");
+            var text = $(this).find(".card-title:contains('" + search + "')");
             if (text.length) $(this).show();
         });
     });
 
     function checkPreviousArrow() {
-        if (OFFSET <= 0){
+        if (OFFSET <= 0) {
             $("#previous-arrow").prop("disabled", true);
         } else {
             $("#previous-arrow").prop("disabled", false);
@@ -189,14 +200,15 @@ $OFFSET = 0;
         OFFSET -= NB_PER_PAGE;
         $("#next-arrow").prop("disabled", false);
         $.ajax({
-            url:"pagination.php",
-            method:"GET",
-            data:{  tagID : <?= $_GET['id'] ?>,
-                nbppage : NB_PER_PAGE,
-                offset : OFFSET
+            url: "pagination.php",
+            method: "GET",
+            data: {
+                tagID: <?= $_GET['id'] ?>,
+                nbppage: NB_PER_PAGE,
+                offset: OFFSET
             },
-            dataType:"json",
-            success:function(posts){
+            dataType: "json",
+            success: function(posts) {
                 displayPosts(posts);
                 checkPreviousArrow();
             }
@@ -204,7 +216,7 @@ $OFFSET = 0;
     });
 
     function checkNextArrow() {
-        if (OFFSET > NB_POST - 2){
+        if (OFFSET > NB_POST - 2) {
             $("#next-arrow").prop("disabled", true);
         } else {
             $("#next-arrow").prop("disabled", false);
@@ -216,21 +228,20 @@ $OFFSET = 0;
         OFFSET += NB_PER_PAGE;
         $("#previous-arrow").prop("disabled", false);
         $.ajax({
-            url:"pagination.php",
-            method:"GET",
-            data:{  tagID : <?= $_GET['id'] ?>,
-                nbppage : NB_PER_PAGE,
-                offset : OFFSET
+            url: "pagination.php",
+            method: "GET",
+            data: {
+                tagID: <?= $_GET['id'] ?>,
+                nbppage: NB_PER_PAGE,
+                offset: OFFSET
             },
-            dataType:"json",
-            success:function(posts){
+            dataType: "json",
+            success: function(posts) {
                 displayPosts(posts);
                 checkNextArrow();
             }
         });
     });
-
-
 </script>
 
 
@@ -238,4 +249,3 @@ $OFFSET = 0;
 include "modals.php";
 require "inc/footer.php";
 ?>
-
